5464dd80ccd9196ed75a16c545251d124bc913f7
menslow <michael.enslow@gmail.com>
2013-01-31 17:02:06 -0800

    added localization to 404 template, got rid of spaces in searchform template

--- wp-content/themes/deathstar/404.php
+++ wp-content/themes/deathstar/404.php
@@ -3,17 +3,12 @@
  * @package WordPress
  * @subpackage 
  */
-
 get_header();
 ?>
-
-	<div id="content">
-
-		<h2>Oops!</h2>
-		<p>We couldn't find the page you were looking for.</p>
-
-	</div>
-
+<div id="content">
+	<h2><?php _e('Oops!'); ?></h2>
+	<p><?php _e('We couldn\'t find the page you were looking for.'); ?></p>
+</div>
 <?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
--- wp-content/themes/deathstar/searchform.php
+++ wp-content/themes/deathstar/searchform.php
@@ -1,6 +1,6 @@
-    <form id="searchform" class="search alignright" method="get" action="/index.php">
-      <fieldset>
-         <input class="text" type="text" name="s" id="s" size="15" />
-         <button class="btn btn_one">search</button>
-      </fieldse>
-     </form>
+<form id="searchform" class="search alignright" method="get" action="/index.php">
+	<fieldset>
+		<input class="text" type="text" name="s" id="s" size="15" />
+		<button class="btn btn_one">search</button>
+	</fieldse>
+</form>
